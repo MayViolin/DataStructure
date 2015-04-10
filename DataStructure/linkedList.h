5e0e3a18e6e69e1e0a64ca46f6579710d50c44cf
wangyouli <wangyouli@neotel.com.cn>
2015-04-10 17:02:00 +0800

    add by wyl

---
+++ DataStructure/linkedList.h
@@ -0,0 +1,53 @@
+#ifndef LINKEDLIST_H_INCLUDED
+#define LINKEDLIST_H_INCLUDED
+
+#endif // LINKEDLIST_H_INCLUDED
+
+typedef struct Node {
+  int data;
+  struct Node *next;
+}Node;
+
+typedef Node LinkedList;
+
+LinkedList * createList(int arr[], int size) {
+  if (0 == size || NULL == arr) {
+    return NULL;
+  }
+  LinkedList* head = (LinkedList*)malloc(sizeof(Node)*size);
+  if (NULL == head) {
+    return NULL;
+  }
+  head->data = size;
+  head->next = NULL;
+  Node* cur = head;
+  int i;
+  for (i = 0; i < size; i++) {
+    Node *temp = (Node*)malloc(sizeof(Node)*size);
+    if (NULL == temp) {
+        return NULL;
+    }
+    temp->data = arr[i];
+    temp->next = cur->next;
+    cur->next = temp;
+    cur = temp;
+  }
+  return head;
+}
+
+void printList(LinkedList* list) {
+  Node* cur = list->next;
+  while (NULL != cur) {
+    printf("%d", cur->data);
+    cur = cur->next;
+  }
+}
+
+void releaseList(LinkedList *list) {
+  Node *cur = list;
+  while (NULL != cur) {
+    Node *temp = cur->next;
+    free(cur);
+    cur = temp;
+  }
+}
---
+++ DataStructure/main.c
@@ -0,0 +1,19 @@
+﻿#include <stdio.h>
+#include <stdlib.h>
+#include "linkedList.h"
+
+int main(void)
+{
+    int a[] = {1,2,3,4,5,6,7,8,9};
+    int size = 9;
+
+    LinkedList *list;
+    list = createList(a,size);
+    if (NULL != list) {
+      printList(list);
+    } else {
+      printf("Error!\n");
+    }
+    releaseList(list);
+    return 0;
+}
